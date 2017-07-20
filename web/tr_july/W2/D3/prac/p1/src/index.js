38d0554748fabfa3ee092393472fd8862d3ed434
Mohit Mehta <mehtamohit987@gmail.com>
2017-07-20 08:35:51 +0530

    back

--- web/tr_july/W2/D3/prac/p1/src/index.js
+++ web/tr_july/W2/D3/prac/p1/src/index.js
@@ -1,9 +1,13 @@
 import _ from 'lodash';
+import '../less/main.less';
+
 function component() {
     var element = document.createElement('div');
-
-   // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    var greet, from;
+    [greet='Hello', from='Dude'] = [undefined, 'webpack'];
+    // Lodash, now imported by this script
+    element.innerHTML = _.join([greet, from], ' ');
+    element.classList.add('alpha');
 
     return element;
   }
--- web/tr_july/W2/D3/prac/p1/webpack.config.js
+++ web/tr_july/W2/D3/prac/p1/webpack.config.js
@@ -1,9 +1,62 @@
 var path = require('path');
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    vendor: ['lodash',],
+    app: './src/index.js'
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  devtool: debug ? "inline-source-map" : "cheap-source-map",
+  module: {
+  rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "less-loader"]
+        })
+      },
+      {
+        test: /\.css$/,
+        use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }
+        ]
+      }
+    ]
+  },
+  plugins: debug ? [
+      new webpack.optimize.CommonsChunkPlugin({
+        name: "vendor",
+        filename: "vendor.js",
+        minChunks: Infinity
+      }),
+      new ExtractTextPlugin("styles.css"),
+    ] : [
+    new webpack.optimize.UglifyJsPlugin({ sourcemap: true,}),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: "vendor",
+        filename: "vendor.js",
+        minChunks: Infinity,
+    }),
+    new ExtractTextPlugin("styles.css"),
+  ],
 };
