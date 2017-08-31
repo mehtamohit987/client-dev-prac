14ae56bf91442a3e09ad183bed26d5b72b34f98e
Mohit Mehta <mehtamohit987@gmail.com>
2017-08-31 22:41:02 +0530

    updated

--- web/tr_july/W2/game-prac/tetris/main.js
+++ web/tr_july/W2/game-prac/tetris/main.js
@@ -3,11 +3,22 @@ const context = canvas.getContext('2d');
  
 context.scale(20,20);
 
-const matrix = [
-    [0,0,0],
-    [1,1,1],
-    [0,1,0]
-];
+function arenaSweep () {
+    let rowCount = 1;
+    outer: for (let y = arena.length - 1; y > 0; --y) {
+        for (let x = 0; x < arena[y].length; ++x) {
+            if (arena[y][x] === 0) {
+            continue outer;
+            }
+        }
+
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+        player.score += rowCount * 10;
+        rowCount *= 2;
+    }
+}
 
 function collide (arena, player) {
     const [m, o] = [player.matrix, player.pos];
@@ -29,11 +40,24 @@ function createMatrix(w, h) {
     return matrix;
 }
 
-function drawMatrix(matrix, offset){
+function createPiece (type) {
+    switch (type) {
+        case 'T': return [ [0, 0, 0], [1, 1, 1], [0, 1, 0] ];
+        case 'O': return [ [2, 2], [2, 2] ];
+        case 'L': return [ [0, 3, 0], [0, 3, 0], [0, 3, 3] ];
+        case 'J': return [ [0, 4, 0], [0, 4, 0], [4, 4, 0] ];
+        case 'I': return [ [0, 0, 0], [5, 5, 5], [0, 5, 0] ];
+        case 'S': return [ [0, 6, 6], [6, 6, 0], [0, 0, 0] ];
+        case 'Z': return [ [7, 7, 0], [0, 7, 7], [0, 0, 0] ];
+        default: return [];
+    }
+}
+
+function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
-                context.fillStyle = 'red';
+                context.fillStyle = colors[value];
                 context.fillRect(x + offset.x,
                                 y + offset.y,
                                 1, 1);
@@ -44,9 +68,31 @@ function drawMatrix(matrix, offset){
 
 const arena = createMatrix(12, 20);
 
+function playerReset() {
+    const pieces = 'ILJOTSZ';
+    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+    player.pos.y = 0;
+    player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
+    if (collide(arena, player)) {
+        arena.forEach(row => row.fill(0));
+        player.score = 0;
+
+    }
+}
+const colors = [
+    null,
+    'red',
+    'blue',
+    'violet',
+    'green',
+    'purple',
+    'orange',
+    'pink'
+];
 const player = {
+    score: 0,
     pos: {x:5, y:5},
-    matrix: matrix
+    matrix: createPiece('T')
 };
 
 function draw () {
@@ -90,7 +136,9 @@ function playerDrop(){
     if (collide(arena, player)) {
         player.pos.y--;
         merge(arena, player);
-        player.pos.y = 0;
+        playerReset();
+        arenaSweep();
+        updateScore();
     }
     dropCounter = 0;
 }
@@ -131,6 +179,10 @@ function update(time=0) {
     requestAnimationFrame(update);
 }
 
+function updateScore () {
+    document.getElementById('score').innerText = player.score;
+}
+
 document.addEventListener('keydown', event => {
     if (event.keyCode === 37) {
         playerMove(-1);
@@ -144,4 +196,5 @@ document.addEventListener('keydown', event => {
         playerRotate(1);
     }
 });
+playerReset();
 update();
